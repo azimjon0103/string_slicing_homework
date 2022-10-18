0bb47f4281ca354b2ae12acc5dbde4bba04706d0
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-18 11:46:05 +0500

    06

--- Slicing06.py
+++ Slicing06.py
@@ -1,10 +1,11 @@
 def main(s,n):
     """
     The s string variable is given. return all characters except n characters from the beginning.
-    Args:
+    Args: 
         s(str): parameter
         n(int): parameter
     Returns:
         str: answer
     """
-    return
+    return s[n:] 
+print(main('apple',1))    
